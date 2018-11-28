d9274e43902add4a6fad6999089dfea718e26965
nitoloz <andrey.zolotin@gmail.com>
2018-11-28 23:45:32 +0100

    * Add integration with firebase auth * Add bike markers statuses * Add dialogs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,49 @@
 /// <reference types="@types/googlemaps" />
-import {AfterContentInit, Component, ViewChild} from '@angular/core';
-import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
-import {Observable} from 'rxjs';
+import {AfterContentInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
+import {combineLatest, Observable} from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {auth, User} from 'firebase/app';
 
 const initialLatitude = 50.119485;
 const initialLongitude = 8.639571;
-const iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+const availableBikeIcon = 'bike_blue.png';
+const rentedBikeIcon = 'bike_grey.png';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterContentInit {
+export class AppComponent implements OnInit, AfterContentInit {
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
+  user: any;
+
+  firebaseUser: User;
 
   private bikesCollection: AngularFirestoreCollection<any>;
-  bikes: Observable<any[]>;
-  selectedMarkerType = 'cycling.png';
+  private userDocument: AngularFirestoreDocument<any>;
 
-  constructor(private afs: AngularFirestore) {
-    this.bikesCollection = afs.collection<any>('bikes');
-    this.bikes = this.bikesCollection.valueChanges();
-    this.bikes.subscribe(bikes => {
-      bikes.forEach(bike => {
-        const bikeLocation = new google.maps.LatLng(bike.location.latitude, bike.location.longitude);
+  constructor(private afStore: AngularFirestore,
+              public afAuth: AngularFireAuth) {
+  }
 
-        const marker = new google.maps.Marker({
-          position: bikeLocation,
-          title: bike.name,
-          map: this.map,
-          icon: {
-            url: iconBase + this.selectedMarkerType,
-            scaledSize: new google.maps.Size(30, 30)
-          }
-        });
-      });
+  ngOnInit() {
+    this.afAuth.user.subscribe(firebaseUser => {
+      this.firebaseUser = firebaseUser;
+      if (firebaseUser) {
+        this.afStore.collection<any>('users').doc(firebaseUser.email)
+          .set({name: firebaseUser.displayName}, {merge: true});
+        this.bikesCollection = this.afStore.collection<any>('bikes');
+        this.userDocument = this.afStore.doc<any>(`users/${this.firebaseUser.email}`);
+
+        combineLatest(this.userDocument.valueChanges(), this.bikesCollection.snapshotChanges())
+          .subscribe(([user, bikes]) => {
+            this.user = user;
+            this.displayBikes(bikes);
+          });
+      }
     });
   }
 
@@ -49,7 +56,78 @@ export class AppComponent implements AfterContentInit {
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProperties);
   }
 
-  addItem(bike: any) {
-    this.bikesCollection.add(bike);
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  private rentBike(bike: any) {
+    if (!this.user.rentedBikeId) {
+      this.afStore.collection<any>('users').doc(this.firebaseUser.email)
+        .set({
+            rentedBikeId: bike.payload.doc.id,
+            rentedBikeName: bike.payload.doc.data().name
+          }, {merge: true}
+        );
+      this.bikesCollection.doc(bike.payload.doc.id).update({rented: true});
+    } else {
+      alert(`Please return first bike ${this.user.rentedBikeName}!`);
+    }
+  }
+
+  private returnBike(bike: any) {
+    if (this.user.rentedBikeId === bike.payload.doc.id) {
+      this.afStore.collection<any>('users').doc(this.firebaseUser.email)
+        .set({
+            rentedBikeId: null,
+            rentedBikeName: null
+          }, {merge: true}
+        );
+      this.bikesCollection.doc(bike.payload.doc.id).update({rented: false});
+    } else {
+      alert(`This is not your bike!`);
+    }
+  }
+
+  private displayBikes(bikes: any[]) {
+    bikes.forEach(bike => {
+      const bikeData = bike.payload.doc.data();
+      const bikeLocation = new google.maps.LatLng(bikeData.location.latitude, bikeData.location.longitude);
+      const infoWindow = new google.maps.InfoWindow({
+        content: `<div>
+                    <h3>Bike ${bikeData.name}</h3>
+                    <button style="float:right;" id="${bike.payload.doc.id}">
+                      ${bikeData.rented ? 'Return!' : 'Rent!'}
+                    </button>
+                  </div>`
+      });
+
+      google.maps.event.addListener(infoWindow, 'domready', () => {
+        document.getElementById(bike.payload.doc.id).addEventListener('click', (e) => {
+          if (bike.payload.doc.data().rented) {
+            this.returnBike(bike);
+          } else {
+            this.rentBike(bike);
+          }
+          infoWindow.close();
+        });
+      });
+
+      const marker = new google.maps.Marker({
+        position: bikeLocation,
+        title: bikeData.name,
+        map: this.map,
+        icon: {
+          url: `assets/${bikeData.rented ? rentedBikeIcon : availableBikeIcon}`,
+          scaledSize: new google.maps.Size(40, 40)
+        }
+      });
+      marker.addListener('click', () => {
+        infoWindow.open(this.map, marker);
+      });
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AngularFireModule} from '@angular/fire';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 import {AppComponent} from './app.component';
 import {environment} from '../environments/environment';
-import {AngularFirestoreModule} from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {AngularFirestoreModule} from '@angular/fire/firestore';
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
