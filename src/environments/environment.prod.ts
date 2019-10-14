7ad473fc0813b110ad57a0c4ea94bfec2df870b1
Andrey <andrey.zolotin@gmail.com>
2019-10-14 12:21:13 +0200

    feat: deploy npm script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBbX9987YShuXWKlHvTrwaJOl8r1eRdVAs',
+    authDomain: 'bikeapp-a6430.firebaseapp.com',
+    databaseURL: 'https://bikeapp-a6430.firebaseio.com',
+    projectId: 'bikeapp-a6430',
+    storageBucket: 'bikeapp-a6430.appspot.com',
+    messagingSenderId: '577374676055'
+  }
 };
